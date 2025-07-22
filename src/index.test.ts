57bf338b4a55efdfdc0766ab6132c7e2e090a729
Starknet Dev <starknetdev@gmail.com>
2025-07-22 15:52:03 +0100

    add no test handler

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'bun:test';
-
-describe('Metagame SDK', () => {
-  it('should have tests implemented', () => {
-    // TODO: Implement actual tests for the SDK
-    expect(true).toBe(true);
-  });
-});
